6b429b894fdef4914cea976cbc59b98237b35796
codingduck106 <65737878+codingduck106@users.noreply.github.com>
2025-08-15 15:41:32 -0700

    removed caesar ciphr

--- main.py
+++ main.py
@@ -7,15 +7,8 @@ from dotenv import load_dotenv
 
 # Load secrets from .env
 
-def caesar_decrypt(text, shift=5):
-    decrypted = ""
-    for char in text:
-        decrypted += chr((ord(char) - shift) % 256)
-    return decrypted
-
-
 load_dotenv()
-DISCORD_TOKEN = caesar_decrypt(os.getenv("DISCORD_TOKEN"))
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
 CANVAS_DOMAIN = os.getenv("CANVAS_DOMAIN")  # e.g. yourschool.instructure.com
 CHANNEL_ID = int(str(os.getenv("CHANNEL_ID")))   # Discord channel ID to post in
@@ -86,4 +79,5 @@ async def on_ready():
     daily_reminder.start()
 
 bot.run(DISCORD_TOKEN) # type: ignore
+
 
